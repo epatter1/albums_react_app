68678c4fac7b3166c9161986d691953023015fc9
Elisha <elishajp20@gmail.com>
2017-11-06 14:20:55 -0600

    Added AlbumList to showcase a class lifecycle component

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,15 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // Create a component
 // return statement and brackets are optional
 const App = () => (
-  <Header headerText={'Do IT!!!'} />
+   <View>
+     <Header headerText={'Albums'} />
+     <AlbumList />
+   </View>
 );
 
 // Render to device
