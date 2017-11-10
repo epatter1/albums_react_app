d47758425c6bee75d2de6deac357d97a9d0ce348
Elisha <elishajp20@gmail.com>
2017-11-10 00:56:54 -0600

    Finished Albums app
    Finished AlbumDetail components: For each Album component retrieved, it produces an AlbumDetail component using a prop. Finished Card and CardSection components. Added Button component that opens a webpage to purchase the ablum

--- albums/index.js
+++ albums/index.js
@@ -6,7 +6,9 @@ import AlbumList from './src/components/AlbumList';
 // Create a component
 // return statement and brackets are optional
 const App = () => (
-   <View>
+	//flex: 1 expands to fill as much content as possible
+   //scrolled fine without flex: 1 for me
+   <View style={{ flex: 1 }}>
      <Header headerText={'Albums'} />
      <AlbumList />
    </View>
--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,19 +1,82 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  //destructering out these prop objects to clean up code
+const { title, artist, thumbnail_image, image, url } = album;
+const {
+  thumbnailStyle,
+  headerContentStyle,
+  thumbnailContainerStyle,
+  headerTextStyle,
+  imageStyle
+  } = styles;
+
   return (
   //renders any children passed to it
 	<Card>
 		<CardSection>
-			<Text>{props.album.title}</Text>
+			<View style={thumbnailContainerStyle}>
+				<Image 
+					style={thumbnailStyle}
+					source={{ uri: thumbnail_image }}
+				/>
+			</View>
+			<View style={headerContentStyle}>
+				<Text style={headerTextStyle}>{title}</Text>
+				<Text>{artist}</Text>
+			</View>
 		</CardSection>
+
+		
+		<CardSection>
+			<Image 
+			style={imageStyle}
+			source={{ uri: image }} 
+			/>
+
+		</CardSection>
+
+		
+		<CardSection>
+	{/* onPress is just what Im calling it - not required here */}
+			<Button onPress={() => Linking.openURL(url)}> 
+				Buy Now!
+			</Button>
+		</CardSection>
+
 	</Card>
 
 	
 	);
+};
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	headerTextStyle: {
+		fontSize: 18
+	},
+	thumbnailStyle: {
+		height: 50,
+		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
 };
 
 export default AlbumDetail;
